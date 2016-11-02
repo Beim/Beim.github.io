237f1028c230db088ec33c7108bd31bc6d537173
beim <beim2015@outlook.com>
2016-11-02 20:52:05 +0800

    Site updated: 2016-11-02 20:52:05

---
+++ js/src/beim/test.js
@@ -0,0 +1,15 @@
+console.log('HELO from next/source/js/src/beim/test.js')
+/*
+let xhr = new XMLHttpRequest()
+xhr.responstType = 'text'
+xhr.open('GET', 'http://beim.site/url?-t;;100')
+
+xhr.onload = (e) => {
+    if (this.status == 200) {
+        console.log(this.responseText)
+    } else {
+        console.log('nononono')
+    }
+}
+xhr.send()
+*/
