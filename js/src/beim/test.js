39b8357793c13bfd682f5b75e7ff42de4b066592
beim <beim2015@outlook.com>
2017-01-08 18:50:14 +0800

    Site updated: 2017-01-08 18:50:13

--- js/src/beim/test.js
+++ js/src/beim/test.js
@@ -1,4 +1,19 @@
 console.log('HELO from next/source/js/src/beim/test.js')
+
+const printable = () => {
+    let header = document.getElementById('header')
+    let postHeader = document.getElementsByClassName('post-header')[0]
+    let postFooter = document.getElementsByClassName('post-footer')[0]
+    let shang = document.getElementById('rewardButton')
+    let comments = document.getElementById('comments')
+    let footer = document.getElementById('footer')
+
+    for (let item of [header, postHeader, postFooter, shang, comments, footer]) {
+        item.className += ' go-away'
+    }
+    print()
+}
+
 /*
 let xhr = new XMLHttpRequest()
 xhr.responstType = 'text'
